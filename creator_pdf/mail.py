bdcc559178c736c433ccd5a7e696e3746d09ea35
EC2 Default User <ec2-user@ip-172-31-29-241.eu-central-1.compute.internal>
2016-06-13 22:55:26 +0000

    trzecie synchro

---
+++ creator_pdf/mail.py
@@ -0,0 +1,31 @@
+mport boto3 session = boto3.session.Session() client = session.client('ses', region_name='eu-west-1') def 
+send_email(to, subject, body):
+    response = client.send_email(
+        Source='167762@wizard.uek.krakow.pl',
+        Destination={
+            'ToAddresses': [
+                to,
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': subject,
+                'Charset': 'utf-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': body,
+                    'Charset': 'utf-8'
+                },
+                'Html': {
+                    'Data': body,
+                    'Charset': 'utf-8'
+                }
+            }
+        },
+        ReplyToAddresses=[
+            '167762@wizard.uek.krakow.pl',
+        ],
+        ReturnPath='167762@wizard.uek.krakow.pl'
+    )
+    return response print send_email('167762@wizard.uek.krakow.pl', 'test', 'test')
